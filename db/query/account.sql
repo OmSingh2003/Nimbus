fc3a216d9ad2cabf3f1d545d320aef85c940160e
Om Singh <omsingh.ailearn@gmail.com>
2025-04-19 02:58:51 +0530

    account sqlc done

--- db/query/account.sql
+++ db/query/account.sql
@@ -5,3 +5,22 @@ INSERT INTO account (
   $1, $2, $3
 )
 RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM account
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM account
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateAccount :exec
+UPDATE account
+SET balance = $2
+WHERE id = $1;
+
+-- name: DeleteAccount :exec
+DELETE FROM account
+WHERE id = $1;
