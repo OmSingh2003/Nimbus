caaf843911790ab97ce878f12f4cca03affaa9a5
Om Singh <omsingh.ailearn@gmail.com>
2025-05-18 01:11:54 +0530

    random.go file for generating random random test values

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,7 +1,26 @@
 package db
 
-import "testing"
-
+import  (
+	"testing"
+	"context"
+ "github.com/OmSingh2003/simple-bank/util"
+	
+	"github.com/stretchr/testify/require"
+)
 func TestCreateAccount(t *testing.T) {
+	arg:= CreateAccountParams{
+		Owner: util.RandomOwner(),
+		Balance: util.RandomMoney(),
+		Currency: util.RandomCurrency(),
+}
+account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+require.NotEmpty(t, account )
 
+require.Equal(t,arg.Owner,account.Owner)
+require.Equal(t, arg.Balance ,account.Balance)
+require.Equal(t ,arg.Currency , account.Currency)
+
+require.NotZero(t, account.ID)
+	require.NotZero(t,account.CreatedAt)
 }
