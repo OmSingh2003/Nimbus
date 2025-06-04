1b13f96d86f59c7a39f90f52617b21ba6e04a49c
Om Singh <omsingh.ailearn@gmail.com>
2025-06-04 12:21:55 +0530

    POST and GET account handler

--- api/server.go
+++ api/server.go
@@ -1,21 +1,32 @@
 package api 
 
 import (
-  "github.com/OmSingh2003/simple-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+	db "github.com/OmSingh2003/simple-bank/db/sqlc"
 )
-// server serves http request for our banking service  
+
+// Server serves HTTP requests for our banking service  
 type Server struct {
-	 store     *db.Store 
-	 router    *gin.Engine
+	store  *db.Store 
+	router *gin.Engine
 }
 
-//NewServer creates a new HTTP server and etup routing 
+// NewServer creates a new HTTP server and setup routing 
 func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
-	router.POST("/accounts",server.createAccount())
-	server.router= router 
+	
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	server.router = router 
 	return server 
 }
 
-func errorResponse(err error) gin.H
+// Start runs the HTTP server on a specific address.
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
