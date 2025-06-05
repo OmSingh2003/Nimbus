8a780b2474da8225ab7b3726c11330c88cfe3980
Om Singh <omsingh.ailearn@gmail.com>
2025-06-05 07:57:45 +0530

    custom params validator for currency

--- api/server.go
+++ api/server.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/OmSingh2003/simple-bank/db/sqlc"
 )
 
@@ -18,13 +20,17 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 	
+	if v , ok  := binding.Validator.Engine().(*validator.Validate) ; ok {
+		v.RegisterValidation("currency",validCurrency)
+	}
+	
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 
 	router.POST("/transfers", server.createTransfer)
 	
-	server.router = router 
+	server.router = router
 	return server 
 }
 
