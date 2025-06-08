abccc1a3fa61a4679f6271377c1ab29fca4cf4a3
Om Singh <omsingh.ailearn@gmail.com>
2025-06-08 12:12:14 +0530

    api to renew the access token

--- api/server.go
+++ api/server.go
@@ -1,63 +1,65 @@
-package api 
+package api
 
 import (
 	"fmt"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
-	"github.com/go-playground/validator/v10"
 	db "github.com/OmSingh2003/simple-bank/db/sqlc"
 	"github.com/OmSingh2003/simple-bank/token"
 	"github.com/OmSingh2003/simple-bank/util"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
-// Server serves HTTP requests for our banking service  
+// Server serves HTTP requests for our banking service
 type Server struct {
-	config util.Config
-	store  db.Store 
-	tokenMaker token.Maker 
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-// NewServer creates a new HTTP server and setup routing 
-func NewServer(config util.Config,store db.Store) (*Server,error) {
-	tokenMaker , err := token.NewPasetoMaker(config.TokenSymmetricKey)
-	if err!=nil {
-		return nil , fmt.Errorf("cannot create token master: %w",err)
+// NewServer creates a new HTTP server and setup routing
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token master: %w", err)
 	}
 	server := &Server{
-		config: config,
-		store: store,
-		tokenMaker:tokenMaker}
-
-	
-	if v , ok  := binding.Validator.Engine().(*validator.Validate) ; ok {
-		v.RegisterValidation("currency",validCurrency)
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
 	}
-	
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	server.setUpRouter()
 
-	return server , nil 
+	return server, nil
 }
 
 func (server *Server) setUpRouter() {
 	router := gin.Default()
-	
+
 	// User routes
 	router.POST("/users", server.createUser)
 	router.POST("/users/login", server.loginUser)
+	router.POST("/token.renew_access", server.renewAccessToken)
 	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
 	// Account routes
 	authRoutes.POST("/accounts", server.createAccount)
 	authRoutes.GET("/accounts/:id", server.getAccount)
 	authRoutes.GET("/accounts", server.listAccount)
-	
+
 	// Transfer routes
 	authRoutes.POST("/transfers", server.createTransfer)
-	
+
 	server.router = router
 }
+
 // Start runs the HTTP server on a specific address.
 func (server *Server) Start(address string) error {
 	return server.router.Run(address)
